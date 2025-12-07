95770e641dd5678a1c4e183d7df1e537a9af7adc
cspss9803 <cspss9803@gmail.com>
2025-12-07 22:01:26 +0800

    修改四捨五入位數到4

--- src/components/SHAPCalculator.vue
+++ src/components/SHAPCalculator.vue
@@ -13,7 +13,7 @@ const sharpeRatio = computed(() => portfolioStore.calcSharpeRatio(0));
     <div class="border border-t-0 border-gray-400 bg-white p-2">
         <h2 class="text-md font-semibold text-blue-700">Sharpe Ratio</h2>
         <span v-if="sharpeRatio !== null" class="mt-2 text-gray-800">
-            {{ sharpeRatio.toFixed(3) }}
+            {{ sharpeRatio.toFixed(4) }}
         </span>
         <span v-else class="mt-2 text-gray-500">尚無資料</span>
     </div>
